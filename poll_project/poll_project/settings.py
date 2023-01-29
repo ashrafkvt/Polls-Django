73e1a1dcbbf5a8b1b4b5b45965f6fbf52e65e941
ashrafkvt <ashrafkvt6197@gmail.com>
2023-01-29 19:18:15 +0530

    minor fix

--- poll_project/poll_project/settings.py
+++ poll_project/poll_project/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+
 load_dotenv(os.path.join(BASE_DIR, '.env.local'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
