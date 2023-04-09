7cf9cd7e42fd4c6519a003d253739d1aac541b55
ashrafkvt <ashrafkvt6197@gmail.com>
2023-04-09 17:04:36 +0530

    Use F() expression

--- poll_project/polls/views.py
+++ poll_project/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.db.models import F
 from django.urls import reverse
 
 from .models import Question, Choice
@@ -31,6 +32,6 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice."
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
